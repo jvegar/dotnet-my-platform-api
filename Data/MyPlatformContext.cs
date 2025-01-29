ccd4768dd03c84155169538a18dddd15042d32b2
Jose Vega <jvegar@uni.pe>
2025-01-29 08:09:56 -0500

    fix(connection-string): Fixed context class in order to get the connection strng from appsettings.json

--- Data/MyPlatformContext.cs
+++ Data/MyPlatformContext.cs
@@ -2,151 +2,157 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using dotnet_my_platform_api.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace dotnet_my_platform_api.Data;
 
 public partial class MyPlatformContext : DbContext
 {
-    public MyPlatformContext()
+  private readonly string _connectionString;
+
+  public MyPlatformContext(IConfiguration configuration)
+      : base(new DbContextOptions<MyPlatformContext>())
+  {
+    _connectionString = configuration.GetConnectionString("DefaultConnection")
+                        ?? throw new ArgumentNullException(nameof(configuration), "Connection string cannot be null.");
+  }
+
+  public virtual DbSet<Education> Educations { get; set; }
+
+  public virtual DbSet<Experience> Experiences { get; set; }
+
+  public virtual DbSet<PersonalInfo> PersonalInfos { get; set; }
+
+  public virtual DbSet<Service> Services { get; set; }
+
+  public virtual DbSet<Skill> Skills { get; set; }
+
+  public virtual DbSet<TechStack> TechStacks { get; set; }
+
+  protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+  {
+    if (!optionsBuilder.IsConfigured)
     {
+      optionsBuilder.UseNpgsql(_connectionString);
     }
+  }
 
-    public MyPlatformContext(DbContextOptions<MyPlatformContext> options)
-        : base(options)
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    modelBuilder.Entity<Education>(entity =>
     {
-    }
+      entity.HasKey(e => e.Id).HasName("education_pkey");
 
-    public virtual DbSet<Education> Educations { get; set; }
+      entity.ToTable("education");
 
-    public virtual DbSet<Experience> Experiences { get; set; }
+      entity.Property(e => e.Id).HasColumnName("id");
+      entity.Property(e => e.DateRange)
+              .HasMaxLength(20)
+              .HasColumnName("date_range");
+      entity.Property(e => e.OrderIndex).HasColumnName("order_index");
+      entity.Property(e => e.Subtitle)
+              .HasMaxLength(255)
+              .HasColumnName("subtitle");
+      entity.Property(e => e.Title)
+              .HasMaxLength(255)
+              .HasColumnName("title");
+    });
 
-    public virtual DbSet<PersonalInfo> PersonalInfos { get; set; }
-
-    public virtual DbSet<Service> Services { get; set; }
-
-    public virtual DbSet<Skill> Skills { get; set; }
-
-    public virtual DbSet<TechStack> TechStacks { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseNpgsql("Host=localhost;Database=my_platform;Username=admin;Password=admin123");
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    modelBuilder.Entity<Experience>(entity =>
     {
-        modelBuilder.Entity<Education>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("education_pkey");
+      entity.HasKey(e => e.Id).HasName("experience_pkey");
 
-            entity.ToTable("education");
+      entity.ToTable("experience");
 
-            entity.Property(e => e.Id).HasColumnName("id");
-            entity.Property(e => e.DateRange)
-                .HasMaxLength(20)
-                .HasColumnName("date_range");
-            entity.Property(e => e.OrderIndex).HasColumnName("order_index");
-            entity.Property(e => e.Subtitle)
-                .HasMaxLength(255)
-                .HasColumnName("subtitle");
-            entity.Property(e => e.Title)
-                .HasMaxLength(255)
-                .HasColumnName("title");
-        });
+      entity.Property(e => e.Id).HasColumnName("id");
+      entity.Property(e => e.Company)
+              .HasMaxLength(255)
+              .HasColumnName("company");
+      entity.Property(e => e.DateRange)
+              .HasMaxLength(20)
+              .HasColumnName("date_range");
+      entity.Property(e => e.OrderIndex).HasColumnName("order_index");
+      entity.Property(e => e.Title)
+              .HasMaxLength(255)
+              .HasColumnName("title");
+    });
 
-        modelBuilder.Entity<Experience>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("experience_pkey");
+    modelBuilder.Entity<PersonalInfo>(entity =>
+    {
+      entity.HasKey(e => e.Id).HasName("personal_info_pkey");
 
-            entity.ToTable("experience");
+      entity.ToTable("personal_info");
 
-            entity.Property(e => e.Id).HasColumnName("id");
-            entity.Property(e => e.Company)
-                .HasMaxLength(255)
-                .HasColumnName("company");
-            entity.Property(e => e.DateRange)
-                .HasMaxLength(20)
-                .HasColumnName("date_range");
-            entity.Property(e => e.OrderIndex).HasColumnName("order_index");
-            entity.Property(e => e.Title)
-                .HasMaxLength(255)
-                .HasColumnName("title");
-        });
+      entity.Property(e => e.Id).HasColumnName("id");
+      entity.Property(e => e.Description).HasColumnName("description");
+      entity.Property(e => e.Email)
+              .HasMaxLength(255)
+              .HasColumnName("email");
+      entity.Property(e => e.Name)
+              .HasMaxLength(100)
+              .HasColumnName("name");
+      entity.Property(e => e.Phone)
+              .HasMaxLength(20)
+              .HasColumnName("phone");
+      entity.Property(e => e.ProjectsCount).HasColumnName("projects_count");
+    });
 
-        modelBuilder.Entity<PersonalInfo>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("personal_info_pkey");
+    modelBuilder.Entity<Service>(entity =>
+    {
+      entity.HasKey(e => e.Id).HasName("services_pkey");
 
-            entity.ToTable("personal_info");
+      entity.ToTable("services");
 
-            entity.Property(e => e.Id).HasColumnName("id");
-            entity.Property(e => e.Description).HasColumnName("description");
-            entity.Property(e => e.Email)
-                .HasMaxLength(255)
-                .HasColumnName("email");
-            entity.Property(e => e.Name)
-                .HasMaxLength(100)
-                .HasColumnName("name");
-            entity.Property(e => e.Phone)
-                .HasMaxLength(20)
-                .HasColumnName("phone");
-            entity.Property(e => e.ProjectsCount).HasColumnName("projects_count");
-        });
+      entity.Property(e => e.Id).HasColumnName("id");
+      entity.Property(e => e.IconPath)
+              .HasMaxLength(255)
+              .HasColumnName("icon_path");
+      entity.Property(e => e.OrderIndex).HasColumnName("order_index");
+      entity.Property(e => e.Title)
+              .HasMaxLength(50)
+              .HasColumnName("title");
+    });
 
-        modelBuilder.Entity<Service>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("services_pkey");
+    modelBuilder.Entity<Skill>(entity =>
+    {
+      entity.HasKey(e => e.Id).HasName("skills_pkey");
 
-            entity.ToTable("services");
+      entity.ToTable("skills");
 
-            entity.Property(e => e.Id).HasColumnName("id");
-            entity.Property(e => e.IconPath)
-                .HasMaxLength(255)
-                .HasColumnName("icon_path");
-            entity.Property(e => e.OrderIndex).HasColumnName("order_index");
-            entity.Property(e => e.Title)
-                .HasMaxLength(50)
-                .HasColumnName("title");
-        });
+      entity.Property(e => e.Id).HasColumnName("id");
+      entity.Property(e => e.IsMainSkill)
+              .HasDefaultValue(false)
+              .HasColumnName("is_main_skill");
+      entity.Property(e => e.LastMonth).HasColumnName("last_month");
+      entity.Property(e => e.LastWeek).HasColumnName("last_week");
+      entity.Property(e => e.Name)
+              .HasMaxLength(100)
+              .HasColumnName("name");
+      entity.Property(e => e.OrderIndex).HasColumnName("order_index");
+      entity.Property(e => e.Percentage).HasColumnName("percentage");
+    });
 
-        modelBuilder.Entity<Skill>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("skills_pkey");
+    modelBuilder.Entity<TechStack>(entity =>
+    {
+      entity.HasKey(e => e.Id).HasName("tech_stack_pkey");
 
-            entity.ToTable("skills");
+      entity.ToTable("tech_stack");
 
-            entity.Property(e => e.Id).HasColumnName("id");
-            entity.Property(e => e.IsMainSkill)
-                .HasDefaultValue(false)
-                .HasColumnName("is_main_skill");
-            entity.Property(e => e.LastMonth).HasColumnName("last_month");
-            entity.Property(e => e.LastWeek).HasColumnName("last_week");
-            entity.Property(e => e.Name)
-                .HasMaxLength(100)
-                .HasColumnName("name");
-            entity.Property(e => e.OrderIndex).HasColumnName("order_index");
-            entity.Property(e => e.Percentage).HasColumnName("percentage");
-        });
+      entity.Property(e => e.Id).HasColumnName("id");
+      entity.Property(e => e.IconPath)
+              .HasMaxLength(255)
+              .HasColumnName("icon_path");
+      entity.Property(e => e.Name)
+              .HasMaxLength(100)
+              .HasColumnName("name");
+      entity.Property(e => e.OrderIndex).HasColumnName("order_index");
+      entity.Property(e => e.Url)
+              .HasMaxLength(255)
+              .HasColumnName("url");
+    });
 
-        modelBuilder.Entity<TechStack>(entity =>
-        {
-            entity.HasKey(e => e.Id).HasName("tech_stack_pkey");
+    OnModelCreatingPartial(modelBuilder);
+  }
 
-            entity.ToTable("tech_stack");
-
-            entity.Property(e => e.Id).HasColumnName("id");
-            entity.Property(e => e.IconPath)
-                .HasMaxLength(255)
-                .HasColumnName("icon_path");
-            entity.Property(e => e.Name)
-                .HasMaxLength(100)
-                .HasColumnName("name");
-            entity.Property(e => e.OrderIndex).HasColumnName("order_index");
-            entity.Property(e => e.Url)
-                .HasMaxLength(255)
-                .HasColumnName("url");
-        });
-
-        OnModelCreatingPartial(modelBuilder);
-    }
-
-    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+  partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
---
+++ Migrations/20250129130502_InitialCreate.cs
@@ -0,0 +1,135 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace dotnet_my_platform_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "education",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    date_range = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    title = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    subtitle = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    order_index = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("education_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "experience",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    date_range = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: false),
+                    title = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    company = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    order_index = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("experience_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "personal_info",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    email = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    phone = table.Column<string>(type: "character varying(20)", maxLength: 20, nullable: true),
+                    description = table.Column<string>(type: "text", nullable: true),
+                    projects_count = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("personal_info_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "services",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    icon_path = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    order_index = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("services_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "skills",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    percentage = table.Column<int>(type: "integer", nullable: false),
+                    last_week = table.Column<int>(type: "integer", nullable: true),
+                    last_month = table.Column<int>(type: "integer", nullable: true),
+                    is_main_skill = table.Column<bool>(type: "boolean", nullable: true, defaultValue: false),
+                    order_index = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("skills_pkey", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tech_stack",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    url = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    icon_path = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    order_index = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("tech_stack_pkey", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "education");
+
+            migrationBuilder.DropTable(
+                name: "experience");
+
+            migrationBuilder.DropTable(
+                name: "personal_info");
+
+            migrationBuilder.DropTable(
+                name: "services");
+
+            migrationBuilder.DropTable(
+                name: "skills");
+
+            migrationBuilder.DropTable(
+                name: "tech_stack");
+        }
+    }
+}
